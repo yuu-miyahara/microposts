d08aed7f040a4c8a4e80f5248b4d4702f2d9c147
yuu.miyahara <miyaharayou1116@gmail.com>
2020-04-27 08:14:03 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
 end
